e990f8f9fa4361027294422c114afc0b61e573c5
Andrew <ad13.works@gmail.com>
2023-10-23 02:04:32 +0200

    common logic moved to AbstractModel

--- database/mysql/models/AbstractModel.ts
+++ database/mysql/models/AbstractModel.ts
@@ -1,8 +1,13 @@
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+
 import { TId } from '../../../types';
+import { MySQLDatabase } from '../mysqlDatabase';
 import DatabaseError from '../errors/DatabaseError';
 
-abstract class AbstractModel {
+abstract class AbstractModel<T extends RowDataPacket> {
   protected tableName: string;
+  protected mysqlDB: MySQLDatabase;
+  protected abstract columnsForCreate: string[];
 
   constructor(tableName: string) {
     this.tableName = tableName;
@@ -12,15 +17,75 @@ abstract class AbstractModel {
     throw new DatabaseError(message, status);
   }
 
-  abstract create(data: Record<string, any>): Promise<Record<string, any>>;
+  protected async create(data: Partial<T>): Promise<Partial<T>> {
+    try {
+      const sql = `INSERT INTO ?? (${this.columnsForCreate.join(', ')}) VALUES(?,?,?)`;
 
-  abstract readById(id: TId): Promise<Record<string, any>>;
+      const result = await this.mysqlDB.executeQuery<ResultSetHeader>(sql, [
+        this.tableName,
+        data.email,
+        data.password,
+        data.name,
+      ]);
 
-  abstract read(conditions: Record<string, any>): Promise<Record<string, any>[]>;
+      const createdItem = await this.readById(result.insertId);
 
-  abstract update(data: Record<string, any>): Promise<Record<string, any>>;
+      return createdItem;
+    } catch (error) {
+      this.throwDatabaseError(`Failed to create ${this.tableName}`, 500);
+    }
+  }
 
-  abstract delete(id: TId): Promise<TId>;
+  protected async readById(id: TId): Promise<Partial<T>> {
+    try {
+      const sql = 'SELECT * FROM ?? WHERE id = ?';
+      const result = await this.mysqlDB.executeQuery<T>(sql, [this.tableName, id]);
+
+      if (!result.length) this.throwDatabaseError('Not found', 404);
+
+      return result[0];
+    } catch (error) {
+      this.throwDatabaseError(`Failed to get by Id: ${id}`, 500);
+    }
+  }
+
+  protected async read(conditions: Partial<T>): Promise<T[]> {
+    try {
+      const sql = 'SELECT * FROM ?? WHERE ?';
+      const result = await this.mysqlDB.executeQuery<T>(sql, [this.tableName, { ...conditions }]);
+      return result;
+    } catch (error) {
+      this.throwDatabaseError(`Failed to find in ${this.tableName}`, 500);
+    }
+  }
+
+  protected async update(data: Partial<T>): Promise<Partial<T>> {
+    try {
+      const sql = 'UPDATE ?? SET ? WHERE id = ?';
+      const result = await this.mysqlDB.executeQuery<ResultSetHeader>(sql, [this.tableName, data, data.id]);
+
+      if (!result.affectedRows) this.throwDatabaseError('Not found', 404);
+
+      const updatedResult = await this.readById(result.insertId);
+
+      return updatedResult;
+    } catch (error) {
+      this.throwDatabaseError(`Failed to update ${this.tableName}: ${data.id}`, 500);
+    }
+  }
+
+  protected async delete(id: TId): Promise<TId> {
+    try {
+      const sql = 'DELETE FROM ?? WHERE id = ?';
+      const result = await this.mysqlDB.executeQuery<ResultSetHeader>(sql, [this.tableName, id]);
+
+      if (!result.affectedRows) this.throwDatabaseError('Not found', 404);
+
+      return id;
+    } catch (error) {
+      this.throwDatabaseError(`Failed to delete: ${this.tableName}: ${id}`, 500);
+    }
+  }
 }
 
 export default AbstractModel;
--- database/mysql/models/UserModel.ts
+++ database/mysql/models/UserModel.ts
@@ -1,87 +1,15 @@
-import { ResultSetHeader } from 'mysql2/promise';
-
 import AbstractModel from './AbstractModel';
 import { MySQLDatabase } from '../mysqlDatabase';
 import { TUser } from '../../types';
-import { TId } from '../../../types';
 import { ETables } from '../../utils/tables';
 
-class UserModel extends AbstractModel {
-  private mysqlDB: MySQLDatabase;
+class UserModel extends AbstractModel<TUser> {
+  protected mysqlDB: MySQLDatabase;
+  protected columnsForCreate = ['email', 'password', 'name'];
 
   constructor() {
     super(ETables.Users);
     this.mysqlDB = MySQLDatabase.getInstance();
-  }
-
-  async create(data: Partial<TUser>): Promise<Partial<TUser>> {
-    try {
-      const sql = 'INSERT INTO ?? (email, password, name) VALUES(?,?,?)';
-
-      const result = await this.mysqlDB.executeQuery<ResultSetHeader>(sql, [
-        this.tableName,
-        data.email,
-        data.password,
-        data.name,
-      ]);
-
-      const createdItem = await this.readById(result.insertId);
-
-      return createdItem;
-    } catch (error) {
-      this.throwDatabaseError('Failed to create a user', 500);
-    }
-  }
-
-  async readById(id: TId): Promise<Partial<TUser>> {
-    try {
-      const sql = 'SELECT * FROM ?? WHERE id = ?';
-      const result = await this.mysqlDB.executeQuery<TUser>(sql, [this.tableName, id]);
-
-      if (!result.length) this.throwDatabaseError('User not found', 404);
-
-      return result[0];
-    } catch (error) {
-      this.throwDatabaseError(`Failed to get a user by Id: ${id}`, 500);
-    }
-  }
-
-  async read(conditions: Partial<TUser>): Promise<TUser[]> {
-    try {
-      const sql = 'SELECT * FROM ?? WHERE ?';
-      const result = await this.mysqlDB.executeQuery<TUser>(sql, [this.tableName, { ...conditions }]);
-      return result;
-    } catch (error) {
-      this.throwDatabaseError(`Failed to find users: ${conditions}`, 500);
-    }
-  }
-
-  async update(data: Partial<TUser>): Promise<Partial<TUser>> {
-    try {
-      const sql = 'UPDATE ?? SET ? WHERE id = ?';
-      const result = await this.mysqlDB.executeQuery<ResultSetHeader>(sql, [this.tableName, data, data.id]);
-
-      if (!result.affectedRows) this.throwDatabaseError('User not found', 404);
-
-      const updatedResult = await this.readById(result.insertId);
-
-      return updatedResult;
-    } catch (error) {
-      this.throwDatabaseError(`Failed to update user: ${data.id}`, 500);
-    }
-  }
-
-  async delete(id: TId): Promise<TId> {
-    try {
-      const sql = 'DELETE FROM ?? WHERE id = ?';
-      const result = await this.mysqlDB.executeQuery<ResultSetHeader>(sql, [this.tableName, id]);
-
-      if (!result.affectedRows) this.throwDatabaseError('User not found', 404);
-
-      return id;
-    } catch (error) {
-      this.throwDatabaseError(`Failed to delete user: ${error.message}`, 500);
-    }
   }
 }
 
