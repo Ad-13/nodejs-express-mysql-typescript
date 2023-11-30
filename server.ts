f920f136bb723e2799880739a6b976e4c64fc8e2
Andrew <ad13.works@gmail.com>
2023-11-30 15:52:44 +0100

    env-sample added

--- server.ts
+++ server.ts
@@ -5,7 +5,6 @@ dotenv.config({ path: './config/.env' });
 
 import Server from './src/index';
 
-
 const app: Application = express();
 const server: Server = new Server(app);
 const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
